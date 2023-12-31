78e1165d19e3ae927102751768113ccabbd52257
WilliamLovaton <walovaton@yahoo.com.mx>
2023-12-31 10:24:43 -0500

    Upgrade to Red Hat build of Quarkus 3.2.9

--- src/main/java/com/redhat/tam/dummy/DummyService.java
+++ src/main/java/com/redhat/tam/dummy/DummyService.java
@@ -1,6 +1,6 @@
 package com.redhat.tam.dummy;
 
-import javax.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
 public class DummyService {
